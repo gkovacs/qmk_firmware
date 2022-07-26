c2d6d2421c5424a571adef2c44d0656b8eee1545
Geza Kovacs <geza0kovacs@gmail.com>
2022-07-25 17:41:38 -0700

    updated moonlander key mappings

--- keyboards/moonlander/keymaps/geza/keymap.c
+++ keyboards/moonlander/keymaps/geza/keymap.c
@@ -58,24 +58,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           MO(3),                                          MO(3),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_GRAVE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TAB,                                         KC_ENTER,       KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,      
-    KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_TAB,                                                                         KC_ENTER,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
+    KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           RSFT(KC_TAB),                                                                         KC_ENTER,       KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
     KC_LCTRL,       KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
     KC_RCTRL,       KC_UP,          KC_DOWN,        KC_LEFT,        KC_RIGHT,       KC_LALT,                                                                                                        TO(1),          KC_UP,          KC_DOWN,        KC_LEFT,        KC_RIGHT,       KC_RCTRL,       
-    MO(2),          KC_LCTRL,       KC_RGUI,                        RGUI(KC_SPACE), KC_BSPACE,      KC_SPACE
+    MO(2),          KC_LCTRL,       KC_RGUI,                        KC_F24, KC_BSPACE,      KC_SPACE
   ),
   [1] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
     KC_GRAVE,       KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           KC_TAB,                                         KC_ENTER,       KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCOLON,      KC_BSLASH,      
-    KC_LSHIFT,      KC_A,           KC_R,           KC_S,           KC_T,           KC_D,           KC_TAB,                                                                         KC_ENTER,       KC_H,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOTE,       
+    KC_LSHIFT,      KC_A,           KC_R,           KC_S,           KC_T,           KC_D,           RSFT(KC_TAB),                                                                         KC_ENTER,       KC_H,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOTE,       
     LM(4,MOD_LCTL), KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_K,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,      
     LM(4,MOD_RCTL), KC_UP,          KC_DOWN,        KC_LEFT,        KC_RIGHT,       LM(4,MOD_LALT),                                                                                                 TO(0),          KC_UP,          KC_DOWN,        KC_LEFT,        KC_RIGHT,       LM(4,MOD_RCTL), 
-    KC_TRANSPARENT, LM(4,MOD_LCTL), LM(4,MOD_RGUI),                 RGUI(KC_SPACE), KC_BSPACE,      KC_SPACE
+    KC_TRANSPARENT, LM(4,MOD_LCTL), LM(4,MOD_RGUI),                 KC_F24, KC_BSPACE,      KC_SPACE
   ),
   [2] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,        KC_TRANSPARENT, 
-    KC_TILD,        KC_TRANSPARENT, KC_TRANSPARENT, KC_UP,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_LCBR,        KC_RCBR,        KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_HOME,        KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_END,         KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_LPRN,        KC_RPRN,        KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT, 
-    KC_TRANSPARENT, KC_TILD,        KC_PLUS,        KC_UNDS,        KC_EQUAL,       KC_MINUS,                                       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,        KC_UNDS,
+    KC_TILD,        KC_TRANSPARENT, KC_TRANSPARENT, KC_UP,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_LCBR,        KC_RCBR,        KC_BSLASH, KC_COLN, KC_PIPE,
+    KC_TRANSPARENT, KC_HOME,        KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_END,         KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_LPRN,        KC_RPRN,        KC_LBRACKET,    KC_RBRACKET,    KC_DQUO,
+    KC_TRANSPARENT, KC_TILD,        KC_PLUS,        KC_UNDS,        KC_EQUAL,       KC_MINUS,                                       KC_TRANSPARENT, KC_PIPE, KC_LABK,        KC_RABK,        KC_QUES,        KC_RSHIFT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
@@ -97,16 +97,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  //switch (keycode) {
-  //  case RGB_SLD:
-  //    if (record->event.pressed) {
-  //      rgblight_mode(1);
-  //    }
-  //    return false;
-  //}
+  switch (keycode) {
+    // case RGB_SLD:
+    //   if (record->event.pressed) {
+    //     rgblight_mode(1);
+    //   }
+    //   return false;
+    case KC_F24:
+        if (record->event.pressed) {
+          if (IS_LAYER_ON(1)) {
+            layer_off(1);
+          } else {
+            layer_on(1);
+          }
+          register_code(KC_LCTRL);
+          tap_code(KC_SPACE);
+          unregister_code(KC_LCTRL);
+        }
+        return false;
+  }
   return true;
 }
-
 
